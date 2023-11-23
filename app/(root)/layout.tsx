7f1a44ca4ae78ef56374710f2b470747ed445a76
mrbillionn <kyaligonzawarren44@gmail.con>
2023-11-23 19:17:55 +0300

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Utopia",
+  title: "Youtopia",
   description: "A communities application",
 };
 
